7d20bad5815bbcbd1fdb8e51b0a005261802866c
Subrata Bauri <subrata@digitalaptech.com>
2019-10-15 19:35:56 +0530

    Add view layer

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -8,9 +8,11 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        /** @var \Magento\Framework\Controller\Result\Raw $result */
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Hello World!');
-        return $result;
+        // /** @var \Magento\Framework\Controller\Result\Raw $result */
+        // $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        // $result->setContents('Hello World!');
+        // return $result;
+
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
