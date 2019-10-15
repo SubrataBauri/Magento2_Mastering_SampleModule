8cf3f77e01ea3d8e49f9d25ad0aa5ce111bdbcac
Subrata Bauri <subrata@digitalaptech.com>
2019-10-15 13:56:42 +0530

    Model and Resourcemodel

---
+++ Model/ResourceModel/Item/Collection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mastering\SampleModule\Model\ResourceModel\Item;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Mastering\SampleModule\Model\Item;
+use Mastering\SampleModule\Model\Resourcemodel\Item as ItemResource;
+
+class Collection extends AbstractCollection
+{
+    protected $_idFieldName = 'id';
+
+    protected function _construct()
+    {
+        $this->_init(
+            Item::class,
+            ItemResource::class
+        );
+    }
+}
